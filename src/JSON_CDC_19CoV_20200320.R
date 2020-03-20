9fb26d096418baec08c9f7ec726d06b6a45930b9
paullin86 <ppaul80025@gmail.com>
2020-03-20 16:44:15 +0800

    add_CDC_19CoV_Crawler

---
+++ src/JSON_CDC_19CoV_20200320.R
@@ -0,0 +1,24 @@
+# JSON https://data.gov.tw/dataset/118038
+# JSON https://data.gov.tw/dataset/118039
+url <- "https://od.cdc.gov.tw/eic/Weekly_Age_County_Gender_19CoV.json"
+library(jsonlite)
+result <- fromJSON(url)
+# data_time <- max(result$inc_notify_time)
+time <- Sys.time()
+time <- gsub("[^0-9]",replacement="",time) 
+time <- paste0(substr(time,start=1,stop=8),"_",substr(time,start=9,stop=16))
+result$'確定病例數' <- as.integer(result$'確定病例數')
+library(data.table)
+result.dt<-data.table(result)
+class(result.dt)
+str(result.dt)
+colnames(result.dt)
+# result.dt[是否為境外移入=='是',.(.N,Sum=sum(確定病例數)),by='縣市']
+# result.dt[,.(Sum=sum(確定病例數)),by=.(縣市,是否為境外移入)][order(縣市,是否為境外移入)]
+X <- result.dt[是否為境外移入=='是',.(Sum_Inter=sum(確定病例數)),by='縣市']
+Y <- result.dt[是否為境外移入=='否',.(Sum_Local=sum(確定病例數)),by='縣市']
+result_op.dt <- merge(X,Y,all=TRUE)
+# save result
+# readr::write_csv(result,paste0("./data/MOTC_incident_",time,".csv")) #,row.names = FALSE,fileEncoding = "UTF-8"
+write.csv(result,paste0("./data/CDC_19CoV_",time,"_Big5",".csv"),row.names = FALSE)
+write.csv(result_op.dt,paste0("./data/CDC_19CoV_County_",time,"_Big5",".csv"),row.names = FALSE)
