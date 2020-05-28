4fcba0f4e4a84cfaf86039109b7108b79049f209
paullin86 <ppaul80025@gmail.com>
2020-05-28 13:22:16 +0800

    add MOTC data

--- runRcode.R
+++ runRcode.R
@@ -1,4 +1,5 @@
 source('./src/JSON_CDC_19CoV_20200320.R', encoding = 'utf-8')
 source('./src/JSON_MOTC_incident_20200130.R', encoding = 'utf-8')
+source('./src/JSON_MOTC_crow_20200501.R', encoding = 'utf-8')
 source('./src/CSV_NHI_mask_20200215.R', encoding = 'utf-8')
-setwd("D:/R_pro/work/NOTE_of_R_Crawler")
+# setwd("D:/R_pro/work/NOTE_of_R_Crawler")
