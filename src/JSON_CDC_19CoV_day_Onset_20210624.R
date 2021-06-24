6ba62c5a73cca9f6e7f67917c339d5801af84bb2
paullin86 <ppaul80025@gmail.com>
2021-06-24 13:14:41 +0800

    ADD CDC_19CoV_2021-06-24

---
+++ src/JSON_CDC_19CoV_day_Onset_20210624.R
@@ -0,0 +1,69 @@
+rm(list=ls())
+# JSON https://data.cdc.gov.tw/dataset/aagstable-day-19cov
+
+url <- "https://od.cdc.gov.tw/eic/Age_County_Gender_day_19Cov.json"
+library(jsonlite)
+t <- fromJSON(url)
+library(readr)
+result_ori <- read_csv("data/CDC_19CoV_day_Onset_temp_Big5.csv", 
+                       col_types = cols(.default = col_character()), 
+                       locale = locale(encoding = "BIG5"))# View(result)
+library(data.table)
+result_ori.dt <- data.table(result_ori)
+t.dt <- data.table(t)
+colnames(t.dt)
+result_ori.dt$'確定病例數' <- as.integer(result_ori.dt$'確定病例數')
+t.dt$'確定病例數' <- as.integer(t.dt$'確定病例數')
+t.dt[,.(Sum_ALL=sum(確定病例數)),]  # result_ori.dt[,.(Sum_ALL=sum(確定病例數)),]
+# -------------
+# str(result_ori.dt);str(t.dt)
+# diff value
+t2 <- t.dt[!result_ori.dt, on = names(t.dt)]
+t3 <- result_ori.dt[!t.dt, on = names(result_ori.dt)]
+# t_diff <- merge(t2,t3,all = TRUE)  #can't join together
+t_diff <- merge(t2,t3,by=c("確定病名","縣市","鄉鎮","性別","是否為境外移入","年齡層","個案研判日"),all = TRUE)
+t_diff[是否為境外移入=='否'&鄉鎮!='其他',.(Sum_ALL=sum(確定病例數.x)),]  # result_ori.dt[,.(Sum_ALL=sum(確定病例數)),]
+t_diff[是否為境外移入=='否'&鄉鎮=='其他',.(Sum_ALL=sum(確定病例數.x)),]  # result_ori.dt[,.(Sum_ALL=sum(確定病例數)),]
+
+# names(result_ori.dt)<- names(t.dt)
+# 
+time <- Sys.time()
+time <- gsub("[^0-9]",replacement="",time) 
+time <- paste0(substr(time,start=1,stop=4),"-",substr(time,start=5,stop=6),"-",substr(time,start=7,stop=8),"_",substr(time,start=9,stop=12))
+result <- t
+result$'確定病例數' <- as.integer(result$'確定病例數')
+
+result.dt<-data.table(result)
+
+class(result.dt)
+str(result.dt)
+colnames(result.dt)
+result.dt[是否為境外移入=='否'&鄉鎮!='其他',.(Sum_ALL=sum(確定病例數)),]  # result_ori.dt[,.(Sum_ALL=sum(確定病例數)),]
+result.dt[是否為境外移入=='是',.(Sum_ALL=sum(確定病例數)),]  # result_ori.dt[,.(Sum_ALL=sum(確定病例數)),]
+
+# result.dt[是否為境外移入=='是',.(.N,Sum=sum(確定病例數)),by='縣市']
+# result.dt[,.(Sum=sum(確定病例數)),by=.(縣市,是否為境外移入)][order(縣市,是否為境外移入)]
+X <- result.dt[是否為境外移入=='是',.(Sum_Inter=sum(確定病例數)),by=.(縣市,鄉鎮)]
+Y <- result.dt[是否為境外移入=='否',.(Sum_Local=sum(確定病例數)),by=.(縣市,鄉鎮)]
+result_op.dt <- merge(X,Y,all=TRUE)
+
+Y.diff <- t_diff[是否為境外移入=='否'&確定病例數.x!='NA',.(Sum_Local.x=sum(確定病例數.x)),by=.(縣市,鄉鎮)]
+t_diff[是否為境外移入=='否'&確定病例數.x!='NA',.(Sum_ALL=sum(確定病例數.x)),by=.(縣市)] 
+t_diff[是否為境外移入=='否'&確定病例數.x!='NA',.(Sum_ALL.x=sum(確定病例數.x)),] 
+t_diff[是否為境外移入=='否'&確定病例數.y!='NA',.(Sum_ALL.y=sum(確定病例數.y)),] 
+
+# save result
+# readr::write_csv(result,paste0("./data/MOTC_incident_",time,".csv")) #,row.names = FALSE,fileEncoding = "UTF-8"
+savef <- function(time){
+  write.csv(result,paste0("./data/CDC_19CoV_day_Onset_",time,"_Big5",".csv"),row.names = FALSE);
+  write.csv(result,paste0("./data/CDC_19CoV_day_Onset_","temp","_Big5",".csv"),row.names = FALSE);
+  write.csv(result_op.dt,paste0("./data/CDC_19CoV_day_Onset_CountyTown_",time,"_Big5",".csv"),row.names = FALSE);
+  write.csv(t_diff,paste0("./data/CDC_19CoV_day_Onset_diff_",time,"_Big5",".csv"),row.names = FALSE);
+  write.csv(Y.diff,paste0("./data/CDC_19CoV_day_Onset_diff_town_",time,"_Big5",".csv"),row.names = FALSE);
+  print(paste0(paste(unique(result_op.dt$縣市), collapse="、"),"等",length(unique(result_op.dt$縣市)),"縣市，共",t.dt[,.(Sum_ALL=sum(確定病例數)),],"例。"))
+}
+
+if(nrow(t2)>0){
+  savef(time)}else{
+    print("not yet updated")}
+
