de3e354577fc0918d770f2743e1ebcd47d69c67c
paullin86 <ppaul80025@gmail.com>
2022-04-12 02:49:45 +0800

    add CDR_pop data

--- src/JSON_CDR_pop_2021-09-20.R
+++ src/JSON_CDR_pop_2021-09-20.R
@@ -1,21 +1,25 @@
+rm(list=ls())
 # https://sheets.googleapis.com/v4/spreadsheets/1ImBcGTr_Peaj7X2OI2Tydxhm2QFs3JapChDgBLImF3g/values/now?alt=json&key=AIzaSyDWGy_OmiMZgNE81M0_Tba0656P5K8t7E0
 url <- "https://sheets.googleapis.com/v4/spreadsheets/1ImBcGTr_Peaj7X2OI2Tydxhm2QFs3JapChDgBLImF3g/values/now?alt=json&key=AIzaSyDWGy_OmiMZgNE81M0_Tba0656P5K8t7E0"
 library(jsonlite)
 
 ?fromJSON
-t <- fromJSON(url, simplifyVector = FALSE,flatten = TRUE) #,simplifyDataFrame = FALSE  
-t$majorDimension
+# t <- fromJSON(url, simplifyVector = FALSE,flatten = TRUE) #,simplifyDataFrame = FALSE  
+t <- fromJSON(url,flatten = TRUE)
+# t$majorDimension
 class(t$values)
-str(t$values)
-colnames <- as.character(t$values[1])
-t$values[11]
-dim(as.data.frame(t$values))
+# str(t$values)
+colnames <- as.character(t$values[[1]])
+# t$values[11]
+# dim(as.data.frame(t$values))
+length(t$values)
 library(dplyr)
-t.df<-lapply((t$values[2:13234]), as.data.frame())
-t.df <- do.call("rbind", t.df) #%>%distinct()
-  as.matrix(as.character(t$values))
-t$values
-# install.packages("googlesheets4")
-library(googlesheets4)
-t <- read_sheet("https://docs.google.com/spreadsheets/d/1ImBcGTr_Peaj7X2OI2Tydxhm2QFs3JapChDgBLImF3g/edit#gid=0",sheet = "now",)
-t <- read_sheet("1ImBcGTr_Peaj7X2OI2Tydxhm2QFs3JapChDgBLImF3g")
+t.df <- as.data.frame(do.call(rbind, t$values[2:length(t$values)]),stringsAsFactors = FALSE)
+colnames(t.df) <- colnames
+head(t.df)
+result <- t.df
+data_time <- max(result$time)
+time <- gsub("[^0-9]",replacement="",data_time) 
+time <- paste0(substr(time,start=1,stop=8),"_",substr(time,start=9,stop=12))
+# readr::write_csv(result,paste0("./data/MOTC_incident_",time,".csv")) #,row.names = FALSE,fileEncoding = "UTF-8"
+write.csv(result,paste0("./data/CDR_pop_",time,"_Big5",".csv"),row.names = FALSE)
