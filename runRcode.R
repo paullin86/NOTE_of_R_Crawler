6c61ffe504bef58c350473d4e3f8bfa7279271cd
paullin86 <ppaul80025@gmail.com>
2022-04-16 00:49:22 +0800

    modify runRcode

--- runRcode.R
+++ runRcode.R
@@ -1,5 +1,8 @@
-source('./src/JSON_CDC_19CoV_20200320.R', encoding = 'utf-8')
+setwd("D:/R_pro/work/NOTE_of_R_Crawler")
+source('./src/JSON_CDR_pop_2021-09-20.R', encoding = 'utf-8')
 source('./src/JSON_MOTC_incident_20200130.R', encoding = 'utf-8')
-source('./src/JSON_MOTC_crow_20200501.R', encoding = 'utf-8')
-source('./src/CSV_NHI_mask_20200215.R', encoding = 'utf-8')
-# setwd("D:/R_pro/work/NOTE_of_R_Crawler")
+# source('./src/JSON_CDC_19CoV_day_20200615.R', encoding = 'utf-8')
+# source('./src/JSON_CDC_19CoV_day_Onset_20210624.R', encoding = 'utf-8')
+# source('./src/JSON_MOTC_crow_20200501.R', encoding = 'utf-8')
+# source('./src/CSV_NHI_mask_20200215.R', encoding = 'utf-8')
+
